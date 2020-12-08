135eb49c16c275984377e759c51eaa90e62624a8
Subhankar De <subhankar_de@cable.comcast.com>
2020-12-08 13:41:11 -0500

    update the jwt secret

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken'
 import getUserId from '../utils/getUserId'
 
 const token = (userId)=>{
-    return jwt.sign({ userId: userId }, 'thisisasecret', { expiresIn: '7 days' })
+    return jwt.sign({ userId: userId }, process.env.JWT_SECRET, { expiresIn: '7 days' })
 }
 
 export {token as default}
--- src/utils/getUserId.js
+++ src/utils/getUserId.js
@@ -4,7 +4,7 @@ const header = request.request ? request.request.headers.authorization : request
 
     if (header) {
         const token = header.replace('Bearer ', '')
-        const decoded = jwt.verify(token, 'thisisasecret')
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
         return decoded.userId
 
     }
